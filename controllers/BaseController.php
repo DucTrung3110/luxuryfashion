3b0f414523d025613752060e0b6b7bf9a8fc5a6d
Đức Trung <152075075+DucTrung3110@users.noreply.github.com>
2025-07-30 11:36:51 +0700

    Update BaseController.php

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -1,38 +1,33 @@
 <?php
-abstract class BaseController
-{
+abstract class BaseController {
     protected $db;
-
-    public function __construct()
-    {
+    
+    public function __construct() {
         $this->db = Database::getInstance();
     }
-
-    protected function render($view, $data = [])
-    {
+    
+    protected function render($view, $data = []) {
         extract($data);
         include "views/{$view}.php";
     }
-
-    protected function requireLogin()
-    {
+    
+    protected function requireLogin() {
         if (!isLoggedIn()) {
             redirect('?controller=users&action=login');
         }
     }
-
-    protected function requireAdmin()
-    {
+    
+    protected function requireAdmin() {
         if (!isAdmin()) {
             redirect('?controller=home&action=index');
         }
     }
-
-    protected function json($data, $status = 200)
-    {
+    
+    protected function json($data, $status = 200) {
         http_response_code($status);
         header('Content-Type: application/json');
         echo json_encode($data);
         exit();
     }
 }
+?>
